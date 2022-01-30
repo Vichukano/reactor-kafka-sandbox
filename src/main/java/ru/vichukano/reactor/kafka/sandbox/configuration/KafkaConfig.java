9c81d1c77ed450879e99b4ecbc83acf0e74a2056
Vichukano <vichukano@gmail.com>
2022-01-31 01:24:57 +0300

    Create exception handling example

---
+++ src/main/java/ru/vichukano/reactor/kafka/sandbox/configuration/KafkaConfig.java
@@ -0,0 +1,58 @@
+package ru.vichukano.reactor.kafka.sandbox.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+public class KafkaConfig {
+    static final String TOPIC = "reactor-kafka-sandbox-topic-1";
+
+    @Bean
+    public KafkaReceiver<Integer, String> kafkaReceiver() {
+        Map<String, Object> props = Map.of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ConsumerConfig.GROUP_ID_CONFIG, "sandbox-group-id",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 10000
+        );
+        ReceiverOptions<Integer, String> ro = ReceiverOptions.<Integer, String>create(props)
+                .commitBatchSize(1)//Commit every record
+                .commitInterval(Duration.ZERO)
+                .subscription(Collections.singletonList(TOPIC));
+        log.info("Create receiver with properties: {}", props);
+        return KafkaReceiver.create(ro);
+    }
+
+    @Bean
+    public KafkaSender<Integer, String> kafkaSender() {
+        var props = new HashMap<String, Object>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        SenderOptions<Integer, String> senderOptions = SenderOptions
+                .<Integer, String>create(props)
+                .maxInFlight(4);
+        log.info("Create sender with properties: {}", props);
+        return KafkaSender.create(senderOptions);
+    }
+
+}
